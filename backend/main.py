e038b389628bb1b893ac4696d7e46b7929b41cf6
Ethan Brook <ethanbrook314@gmail.com>
2025-08-26 11:13:55 -0400

    fix: remove trailing slash from CORS origin to allow demo site requests

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://loralydemo.netlify.app/"], # frontend URL for demo site
+    allow_origins=["https://loralydemo.netlify.app"], # frontend URL for demo site
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
